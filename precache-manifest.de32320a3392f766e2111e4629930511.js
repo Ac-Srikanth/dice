e696d9ce6bbdc8cdc239dc2b689212c5e652553d
Srikanth Ac <srikanthac24@gmail.com>
2019-11-22 22:56:51 +0530

    Updates

---
+++ precache-manifest.de32320a3392f766e2111e4629930511.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f68b4f1a8a758165cf87d28ebbc8a5b",
+    "url": "/dice/index.html"
+  },
+  {
+    "revision": "1e91d706dc92e91955b1",
+    "url": "/dice/static/css/main.7c63d7f8.chunk.css"
+  },
+  {
+    "revision": "7afd36449b17f8c8ee2f",
+    "url": "/dice/static/js/2.171991f9.chunk.js"
+  },
+  {
+    "revision": "1e91d706dc92e91955b1",
+    "url": "/dice/static/js/main.680438df.chunk.js"
+  },
+  {
+    "revision": "b01800f9e119e3627f1c",
+    "url": "/dice/static/js/runtime-main.3594a71b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/dice/static/media/logo.25bf045c.svg"
+  }
+]);
